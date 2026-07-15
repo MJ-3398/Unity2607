c9c32834332085d2677160530ac5e2beb49a022e
MJ-3398 <g32153851@gmail.com>
2026-07-16 02:40:40 +0900

    update

---
+++ Assets/Scripts/TitleSet.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class TitleSet : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    // Update is called once per frame
+    public void Load()
+    {
+        Debug.Log("Load");
+    }
+
+    public void Setting()
+    {
+        Debug.Log("Setting");
+    }
+    public void Quit()
+    {
+        Debug.Log("Quit");
+    }
+}
